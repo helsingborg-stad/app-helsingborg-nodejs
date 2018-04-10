d3bcc3787ff5099dd4080901973902c0a6f82ec2
Viktor Holtenäs <viktor@ustwo.com>
2018-04-10 12:31:37 +0200

    Fixed eslint errors

--- src/routes/guidegroup.js
+++ src/routes/guidegroup.js
@@ -3,10 +3,6 @@ const { fetchProperties } = require('../services/fetchServices');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
-
 router.get('/property/:id', (req, res, next) => {
   const { id } = req.params;
 
--- src/services/fetchServices.js
+++ src/services/fetchServices.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
 
 // TODO move to .env
-const _API_BASE = 'https://api.helsingborg.se/event/json/wp/v2';
+const API_HOST_URL = 'https://api.helsingborg.se/event/json/wp/v2';
 
 module.exports = {
   async fetchProperties(id) {
-    const response = await fetch(`${_API_BASE}/property?post=${id}`);
+    const response = await fetch(`${API_HOST_URL}/property?post=${id}`);
     if (response.status !== 200) throw new Error('Malformed request');
     const json = await response.json();
     // TODO parse and repackage structure to fit app
