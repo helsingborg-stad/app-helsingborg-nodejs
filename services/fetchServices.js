bb345e567ed7fb0eed70f9f6e11dc45be500d43a
Viktor Holtenäs <viktor@ustwo.com>
2018-04-10 11:22:34 +0200

    Forwarding guidegroup properties

--- services/fetchServices.js
+++ services/fetchServices.js
@@ -1,13 +1,12 @@
+const fetch = require("node-fetch");
+
 //TODO move to .env
-const _API_BASE = "";
+const _API_BASE = "https://api.helsingborg.se/event/json/wp/v2";
 
 module.exports = {
   async fetchProperties(id) {
     const response = await fetch(`${_API_BASE}/property?post=${id}`);
-    console.log("Response", response);
     const json = await response.json();
-    console.log("JSON: ", json);
-
     return json;
   }
 };
