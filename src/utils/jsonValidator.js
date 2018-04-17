c9b7b40b380f995fce60bebbfacd7cd430e44174
Viktor Holtenäs <viktor@ustwo.com>
2018-04-17 09:17:06 +0200

    Extracted opening hours json schema

--- src/utils/jsonValidator.js
+++ src/utils/jsonValidator.js
@@ -5,10 +5,12 @@ const v = new Validator();
 const guideGroup = require('../json-schemas/guideGroup');
 const location = require('../json-schemas/location');
 const images = require('../json-schemas/images');
+const openingHour = require('../json-schemas/openingHour');
 
 v.addSchema(guideGroup, '/guideGroup');
 v.addSchema(location, '/location');
 v.addSchema(images, '/images');
+v.addSchema(openingHour, '/openingHour');
 
 module.exports = {
   validate: (data, schema) => {
