73e02597398923599b0ed47c7b851de9eda2a348
Viktor Holtenäs <viktor@ustwo.com>
2018-04-27 15:53:45 +0200

    Sorted out TS exports

--- src/routes/guides.ts
+++ src/routes/guides.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-const { fetchAllGuides } = require('../utils/fetchUtils');
-const logApp = require('debug')('app');
+import express from "express";
+import { fetchAllGuides } from "../utils/fetchUtils";
+const logApp = require("debug")("app");
 
 const router = express.Router();
 
-router.get('', (req, res, next) => {
+router.get("", (req, res, next) => {
   //TODO add express-validator
-  logApp('Router received request');
+  logApp("Router received request");
   const { lang } = req.query;
 
   fetchAllGuides(lang)
--- src/utils/fetchUtils.ts
+++ src/utils/fetchUtils.ts
@@ -7,61 +7,61 @@ const logWarn = debug("warn");
 // TODO move to .env
 const API_HOST_URL = "https://api.helsingborg.se/event/json/wp/v2";
 
-module.exports = {
-  async fetchProperties(id: number) {
-    const response = await fetch(`${API_HOST_URL}/property?post=${id}`);
-    if (!response.ok) throw new Error("Malformed request");
-    const json = await response.json();
-    // TODO parse and repackage structure to fit app
-    return json;
-  },
+async function fetchProperties(id: number) {
+  const response = await fetch(`${API_HOST_URL}/property?post=${id}`);
+  if (!response.ok) throw new Error("Malformed request");
+  const json = await response.json();
+  // TODO parse and repackage structure to fit app
+  return json;
+}
 
-  async fetchAllGuideGroups(lang: string) {
-    let url = `${API_HOST_URL}/guidegroup?_embed`;
-    if (lang) url += `&lang=${lang}`;
-    logApp(`fetching from:${url}`);
+async function fetchAllGuideGroups(lang: string) {
+  let url = `${API_HOST_URL}/guidegroup?_embed`;
+  if (lang) url += `&lang=${lang}`;
+  logApp(`fetching from:${url}`);
 
-    const response = await fetch(url);
-    if (!response.ok) throw new Error("Malformed request");
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Malformed request");
 
-    const guideGroupArray = await response.json();
+  const guideGroupArray = await response.json();
 
-    const resultArray: any[] = [];
+  const resultArray: any[] = [];
 
-    guideGroupArray.forEach((item: any) => {
-      try {
-        resultArray.push(parseGuideGroup(item));
-      } catch (err) {
-        // Discard item
-        logWarn("Failed to parse item:", err);
-      }
-    });
+  guideGroupArray.forEach((item: any) => {
+    try {
+      resultArray.push(parseGuideGroup(item));
+    } catch (err) {
+      // Discard item
+      logWarn("Failed to parse item:", err);
+    }
+  });
 
-    return resultArray;
-  },
+  return resultArray;
+}
 
-  async fetchAllGuides(lang: string) {
-    let url = `${API_HOST_URL}/guide?_embed`;
-    if (lang) url += `&lang=${lang}`;
-    logApp(`sent fetching request to:${url}`);
+async function fetchAllGuides(lang: string) {
+  let url = `${API_HOST_URL}/guide?_embed`;
+  if (lang) url += `&lang=${lang}`;
+  logApp(`sent fetching request to:${url}`);
 
-    const response = await fetch(url);
-    logApp(`received fetching response from:${url}`);
-    if (!response.ok) throw new Error("Malformed request");
+  const response = await fetch(url);
+  logApp(`received fetching response from:${url}`);
+  if (!response.ok) throw new Error("Malformed request");
 
-    const guidesJson = await response.json();
+  const guidesJson = await response.json();
 
-    const guides: any = [];
-    guidesJson.forEach((item: any) => {
-      try {
-        guides.push(parseGuide(item));
-      } catch (err) {
-        // Discard item
-        logWarn("Failed to parse item:", err);
-      }
-    });
-    logApp("Guides parsing complete");
+  const guides: any = [];
+  guidesJson.forEach((item: any) => {
+    try {
+      guides.push(parseGuide(item));
+    } catch (err) {
+      // Discard item
+      logWarn("Failed to parse item:", err);
+    }
+  });
+  logApp("Guides parsing complete");
 
-    return guides;
-  }
-};
+  return guides;
+}
+
+export { fetchAllGuideGroups, fetchAllGuides, fetchProperties };
