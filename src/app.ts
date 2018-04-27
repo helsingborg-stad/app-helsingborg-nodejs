86eb0a6cc9b2f1c2d0fd9ca5c453e16c3e2d6243
Viktor Holtenäs <viktor@ustwo.com>
2018-04-27 16:01:04 +0200

    Typed ErrorRequestHandler

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,34 @@
-import express from 'express';
-import logger from 'morgan';
-import debug from 'debug';
-import http from 'http';
-import guideGroupRouter from './routes/guidegroup';
-import guidesRouter from './routes/guides';
-import { normalizePort } from './utils/serverUtils';
+import express, { ErrorRequestHandler } from "express";
+import logger from "morgan";
+import debug from "debug";
+import http from "http";
+import guideGroupRouter from "./routes/guidegroup";
+import guidesRouter from "./routes/guides";
+import { normalizePort } from "./utils/serverUtils";
+import { RequestHandlerParams } from "express-serve-static-core";
 
-if(process.env.NODE_ENV === "production")
-{
-  require('newrelic');
+if (process.env.NODE_ENV === "production") {
+  require("newrelic");
 }
 
 const app = express();
-const logApp = debug('app');
+const logApp = debug("app");
 
-const port = normalizePort(process.env.PORT || '5000');
+const port = normalizePort(process.env.PORT || "5000");
 
-// eslint-disable-next-line no-unused-vars
-function errorHandler(err, req, res, next) {
-  res.status(500).send({ error: 'Something failed.' });
-}
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(500).send({ error: "Something failed." });
+};
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /**
  * ROUTES
  */
-app.use('/guidegroup', guideGroupRouter);
-app.use('/guide', guidesRouter);
+app.use("/guidegroup", guideGroupRouter);
+app.use("/guide", guidesRouter);
 
 /**
  * ERROR handler
@@ -41,20 +40,20 @@ app.use(errorHandler);
  */
 const server = http.createServer(app);
 function onError(error: NodeJS.ErrnoException) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       // eslint-disable-next-line no-console
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       // eslint-disable-next-line no-console
       console.error(`${bind} is already in use`);
       process.exit(1);
@@ -66,10 +65,10 @@ function onError(error: NodeJS.ErrnoException) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
   logApp(`Listening on ${bind}`);
 }
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
