a91a947837925a3081880d1d64868c1556d5bd8c
Viktor Holtenäs <viktor@ustwo.com>
2018-05-04 10:18:39 +0200

    Fixed tslint error

--- src/utils/jsonValidator.ts
+++ src/utils/jsonValidator.ts
@@ -1,12 +1,13 @@
 import Ajv from "ajv";
 
+import jsonSchema = require("ajv/lib/refs/json-schema-draft-06.json");
 import guideGroup from "../../json-schemas/guideGroup.json";
 import pointProperty from "../../json-schemas/pointProperty.json";
 
 const options: Ajv.Options = { verbose: true };
 const ajv = new Ajv(options);
 
-ajv.addMetaSchema(require("ajv/lib/refs/json-schema-draft-06.json"));
+ajv.addMetaSchema(jsonSchema);
 
 // Add schemas
 ajv.addSchema(guideGroup, "guideGroup");
