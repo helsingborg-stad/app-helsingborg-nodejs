8aee845336889ad44b1627f763d350789db250b8
Viktor Holtenäs <viktor@ustwo.com>
2018-05-29 11:22:27 +0200

    Added script for regenerating JSON-SCHEMAS
    Also generated schemas for the INavigation types.

--- src/utils/jsonValidator.ts
+++ src/utils/jsonValidator.ts
@@ -1,15 +1,15 @@
 import Ajv from "ajv";
 
 import jsonSchema = require("ajv/lib/refs/json-schema-draft-06.json");
-import beacon from "../../json-schemas/beacon.json";
-import contentObject from "../../json-schemas/contentObject.json";
-import guide from "../../json-schemas/guide.json";
-import guideGroup from "../../json-schemas/guideGroup.json";
-import link from "../../json-schemas/link.json";
-import location from "../../json-schemas/location.json";
-import mediaContent from "../../json-schemas/mediaContent.json";
-import pointProperty from "../../json-schemas/pointProperty.json";
-import position from "../../json-schemas/position.json";
+import beacon from "../../json-schemas/IBeacon.json";
+import contentObject from "../../json-schemas/IContentObject.json";
+import guide from "../../json-schemas/IGuide.json";
+import guideGroup from "../../json-schemas/IGuideGroup.json";
+import link from "../../json-schemas/ILink.json";
+import location from "../../json-schemas/ILocation.json";
+import mediaContent from "../../json-schemas/IMediaContent.json";
+import pointProperty from "../../json-schemas/IPointProperty.json";
+import position from "../../json-schemas/IPosition.json";
 
 const options: Ajv.Options = { verbose: true };
 const ajv = new Ajv(options);
